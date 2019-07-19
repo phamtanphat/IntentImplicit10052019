d8eb7bab072cb67a54953bf435f93cd262614cd8
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-19 20:24:33 +0700

    a

---
+++ app/src/main/java/khoapham/ptp/phamtanphat/intendimplicit10052019/MainActivity.java
@@ -0,0 +1,59 @@
+package khoapham.ptp.phamtanphat.intendimplicit10052019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnWifi;
+    int i = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnWifi = findViewById(R.id.buttonCheckwifi);
+
+        btnWifi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    private boolean isConnectingToInternet() {
+        ConnectivityManager connectivity = (ConnectivityManager) MainActivity.this
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+
+        }
+        return false;
+    }
+
+    private boolean isConnectingToWifi() {
+        ConnectivityManager connManager = (ConnectivityManager) MainActivity.this
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager
+                .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (mWifi != null) {
+            if (mWifi.getState() == NetworkInfo.State.CONNECTED)
+                return true;
+        }
+        return false;
+    } }
+}
